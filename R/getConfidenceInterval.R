431bd7658830b3a1eb9e29ba8c9dcf826177b749
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-11-25 17:15:10 +0000

    changing CI

--- R/getConfidenceInterval.R
+++ R/getConfidenceInterval.R
@@ -19,15 +19,11 @@ getConfidenceInterval <- function(table, nsr, confidenceInterval = 95){
     counts$upper_asr_ci <- counts$upper_csr_ci/nsr
 
   } else if (counts$marker_first == 0){
-    counts$marker_first <-  0.5
-    counts$lower_csr_ci <- stats::qbeta(confidenceIntervalLevel, counts$index_first + 0.5, counts$marker_first + 0.5)
-    counts$upper_csr_ci <- stats::qbeta(1-confidenceIntervalLevel, counts$index_first + 0.5, counts$marker_first + 0.5)
+    counts$lower_csr_ci <- Inf
+    counts$upper_csr_ci <- Inf
 
-    counts$lower_csr_ci <- counts$lower_csr_ci/(1-counts$lower_csr_ci)
-    counts$upper_csr_ci <- counts$upper_csr_ci/(1-counts$upper_csr_ci)
-
-    counts$lower_asr_ci <- counts$lower_csr_ci/nsr
-    counts$upper_asr_ci <- counts$upper_csr_ci/nsr
+    counts$lower_asr_ci <- Inf
+    counts$upper_asr_ci <- Inf
 
   } else {
     counts$lower_csr_ci <- stats::qbeta(confidenceIntervalLevel, counts$index_first + 0.5, counts$marker_first + 0.5)
